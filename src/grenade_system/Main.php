5840599e39b4ca73a0a3192085b62477888739dc
suinua <maatinosippo@gmail.com>
2020-07-15 23:19:50 +0900

    不要なコードを削除

--- src/grenade_system/Main.php
+++ src/grenade_system/Main.php
@@ -13,7 +13,6 @@ class Main extends PluginBase
 {
     public function onEnable() {
 
-        $this->getLogger()->info("GrenadeSystemを読み込みました");
         Entity::registerEntity(SmokeGrenadeEntity::class, true, ['SmokeGrenade']);
         Entity::registerEntity(FragGrenadeEntity::class, true, ['FragGrenade']);
         Entity::registerEntity(FlameBottleEntity::class, true, ['FlameBottle']);
