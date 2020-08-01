dd2c149d906e6310474d3bb4a15a3c3d17d4b6cf
suinua <maatinosippo@gmail.com>
2020-08-01 16:48:27 +0900

    fromGrenade

--- src/grenade_system/pmmp/items/GrenadeItem.php
+++ src/grenade_system/pmmp/items/GrenadeItem.php
@@ -4,9 +4,27 @@
 namespace grenade_system\pmmp\items;
 
 
+use grenade_system\models\FlameBottle;
+use grenade_system\models\FragGrenade;
+use grenade_system\models\Grenade;
+use grenade_system\models\SmokeGrenade;
 use pocketmine\item\Item;
 
 abstract class GrenadeItem extends Item
 {
+    static function fromGrenade(Grenade $grenade): ?GrenadeItem {
+        switch ($grenade::NAME) {
+            case FlameBottle::NAME:
+                return new FlameBottleItem();
+                break;
+            case FragGrenade::NAME:
+                return new FragGrenadeItem();
+                break;
+            case SmokeGrenade::NAME:
+                return new SmokeGrenadeItem();
+                break;
+        }
 
+        return null;
+    }
 }
