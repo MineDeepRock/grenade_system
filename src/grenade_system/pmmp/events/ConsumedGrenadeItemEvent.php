b097916f17e841106747748349e13d7327cd10d4
suinua <maatinosippo@gmail.com>
2020-08-03 15:06:34 +0900

    add ConsumedGrenadeItemEvent

---
+++ src/grenade_system/pmmp/events/ConsumedGrenadeItemEvent.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace grenade_system\pmmp\events;
+
+
+use grenade_system\models\Grenade;
+use pocketmine\event\Event;
+use pocketmine\Player;
+
+class ConsumedGrenadeItemEvent extends Event
+{
+    private $owner;
+    private $grenade;
+
+    public function __construct(Player $owner, Grenade $grenade) {
+        $this->owner = $owner;
+        $this->grenade = $grenade;
+    }
+
+    /**
+     * @return Player
+     */
+    public function getOwner(): Player {
+        return $this->owner;
+    }
+
+    /**
+     * @return Grenade
+     */
+    public function getGrenade(): Grenade {
+        return $this->grenade;
+    }
+}
